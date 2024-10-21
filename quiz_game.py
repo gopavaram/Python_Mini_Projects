1ec944e6e3c258e560c4870d87b2637951c33a8e
Varun Reddy Gopavaram <varun.gopavaram@gmail.com>
2024-10-21 17:04:05 -0400

    Code updated

--- quiz_game.py
+++ quiz_game.py
@@ -1,1 +1,48 @@
+print("Welcome to the Quiz game.")
+
+print("Would you like to play the game?")
+
+playgame= input("Type Yes to continue and No to quit: ")
+
+if playgame.lower()!="yes": #Boolean checker 
+    quit()
+    
+
+print("Okay! Let's play :)")
+
+score=0
+print(f"Your current score as you start your game is: {score}")
+
+answer= input("What does CPU stand for? ").lower() #Question 1
+if answer == "central processing unit":
+    print("Correct!")  
+    score+=1
+else:
+    print("Incorrect :(")
+    
+answer= input("What does PSU stand for? ") #Question 2
+if answer.lower() == "psu":
+    print("Correct!")
+    score+=1
+else:
+    print("Incorrect :(")
+    
+answer= input("What does RAM stand for? ") #Question 3
+if answer.lower() == "random access memory":
+    print("Correct!")
+    score=score+1
+else:
+    print("Incorrect :(")
+    
+
+answer= input("What does GPU stand for? ") #Question 4
+if answer.lower() == "graphics processing unit":
+    print("Correct!")   
+    score=score+1
+else:
+    print("Incorrect :(")
+    
+print(f"Your total score for the test is {score} and you got {score} questions correct")
+print(f"you got {(score/4)*100}% of the questions correct")
+print("Game over. Thank you taking the Quiz!!!")
 
